8911364338a0742b4ceca837c6323ac85df1c3f3
Darkyun991 <costinasdan35@yahoo.com>
2020-03-27 19:28:31 +0200

    Revert "Changed runtim library configuration"
    This reverts commit d94718d822869507b0b5ca819f44654e95096aed.

--- ParseTest/maps/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2020.03.18" name="tileset" tilewidth="32" tileheight="32" tilecount="2128" columns="16">
- <image source="tileset.png" width="512" height="4256"/>
-</tileset>
